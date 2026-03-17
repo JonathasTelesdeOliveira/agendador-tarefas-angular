3893c95e9afb5dcc8aadc33a342145cfc31d5efe
JonathasTelesdeOliviera <jonathasteles777@gmail.com>
2026-03-17 06:48:15 -0300

    Adicionando logica para entrada dados do formulario

--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -7,14 +7,24 @@ import {MatDividerModule} from '@angular/material/divider';
 import { PassowordField } from '../../shared/components/passoword-field/passoword-field';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-
-
-
+import { FormGroup, FormBuilder, FormControl} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatSelectModule, MatIconModule, MatDividerModule, PassowordField, MatFormFieldModule, MatInputModule],
+  imports: [
+    MatCardModule, 
+    MatButtonModule, 
+    MatSelectModule,
+    MatIconModule, 
+    MatDividerModule, 
+    PassowordField, 
+    MatFormFieldModule, 
+    MatInputModule, 
+    ReactiveFormsModule
+  ],
+
   templateUrl: './register.html',
   styleUrls: ['./register.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
@@ -23,4 +33,48 @@ import { MatInputModule } from '@angular/material/input';
 })
 export class Register {
   title = 'agendador-tarefas'
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      fullName: [''],
+      email: [''],
+      password: ['']
+    })
+  }
+
+  get passowordControl(): FormControl{
+    return this.form.get('password') as FormControl
+  }
+
+  submit() {
+    console.log(this.form.value)
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/app/shared/components/passoword-field/passoword-field.ts
+++ src/app/shared/components/passoword-field/passoword-field.ts
@@ -1,20 +1,32 @@
-import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component, Input, signal} from '@angular/core';
 import {MatButtonModule} from '@angular/material/button';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatInputModule} from '@angular/material/input';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'input-passoword-field',
-  imports: [MatFormFieldModule, MatInputModule, MatButtonModule, MatIconModule],
+  imports: [
+    MatFormFieldModule, 
+    MatInputModule, 
+    MatButtonModule, 
+    MatIconModule,
+    ReactiveFormsModule
+  ],
+
   templateUrl: './passoword-field.html',
   styleUrl: './passoword-field.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
+
 export class PassowordField {
-    hide = signal(true);
+  hide = signal(true);
   clickEvent(event: MouseEvent) {
     this.hide.set(!this.hide());
     event.stopPropagation();
   }
+
+  @Input({required: true}) control!: FormControl; 
+
 }
